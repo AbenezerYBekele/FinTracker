314b59d5889b1f1447737669ad793af09a22258a
Abenezer Y Bekele <113382918+AbenezerYBekele@users.noreply.github.com>
2025-08-31 11:53:35 -0500

    Update API.js

--- components/API.js
+++ components/API.js
@@ -9,7 +9,7 @@ const parseReceiptText = (text) => {
     return { title, amount, type };
 };
 
-const GEMINI_API_KEY = 'AIzaSyBMuvRvXZAmQddiZN6GgLTtUettVSPqWnI';
+const GEMINI_API_KEY = ' ';
 export const extractReceiptDataAPI = async (base64Image) => {
     const response = await fetch(
         "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=" + GEMINI_API_KEY,
